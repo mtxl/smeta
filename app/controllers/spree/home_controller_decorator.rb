fb8b71dd93b59502a72d8fbb45caea336b0a3b17
Yuri Beloborodov <yubeloborodov@gmail.com>
2015-04-16 12:05:38 +0500

    Closes: #812

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,7 +2,6 @@ Spree::HomeController.class_eval do
   def index
     @home_controller = true
     @searcher = build_searcher(params.merge(include_images: true))
-    @products = @searcher.retrieve_products
     @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
 end
